b29ec145d8c9533136044763618923054dc7185a
Edu Ruiz <edu.ruizzo@gmail.com>
2022-11-04 20:10:47 +0100

    configuration commands and docker-compose personalization

--- commands.sh
+++ commands.sh
@@ -40,3 +40,29 @@ $KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --des
 # RESETTING OFFSETS (also --to-earliest --to-latest --to-datetime --shift-by n)
 $KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --reset-offsets \
 --topic getting-started --group cli-consumer --to-offset 1 --execute
+
+# take a peek into the broker process to see these threads:
+KAFKA_PID=$(jps -l | grep kafka.Kafka | awk '{print $1}') jstack $KAFKA_PID | grep data-plane-kafka-request-handler
+
+
+# Lets use the --describe switch to list the dynamic value. The property name passed to --describe
+# is optional. If omitted, all configuration entries will be shown.
+kafka-configs.sh --bootstrap-server localhost:9092 --entity-type brokers --entity-name 0 --describe num.io.threads
+
+# Let’s change the num.io.threads value by invoking the following command: (to broker 0)
+kafka-configs.sh --bootstrap-server localhost:9092 --entity-type brokers --entity-name 0 --alter --add-config num.io.threads=4
+
+# Let’s apply the num.io.threads.setting to the entire cluster.
+kafka-configs.sh --bootstrap-server localhost:9092 --entity-type brokers --entity-default --alter --add-config num.io.threads=4
+
+# remove the per-broker entry.
+kafka-configs.sh --bootstrap-server localhost:9092 --entity-type brokers --entity-name 0 --alter --delete-config num.io.threads
+
+# revert the configuration to its original state by deleting the cluster-wide entry:
+kafka-configs.sh --bootstrap-server [::1]:9092 --entity-type brokers --entity-default --alter --delete-config num.io.threads
+
+# apply an override for flush.messages for the test.topic.config topic.
+kafka-configs.sh --bootstrap-server [::1]:9092 --entity-type topics --entity-name test.topic.config --alter --add-config flush.messages=100
+kafka-configs.sh --bootstrap-server [::1]:9092 --entity-type topics --entity-name test.topic.config --describe flush.messages
+# back to default
+kafka-configs.sh --bootstrap-server [::1]:9092 --entity-type topics --entity-name test.topic.config --alter --delete-config flush.messages
